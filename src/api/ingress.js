1543303766f4886dc2f440165b071e3ac36006e3
神霄三华 <87976956@qq.com>
2021-10-14 00:22:07 +0800

    21 创建ingress页面

--- src/api/ingress.js
+++ src/api/ingress.js
@@ -1,9 +1,18 @@
 import request from '@/utils/request'
 
-// 获取所有 ns列表
+// 获取ingress列表
 export function getIngressList(ns) {
   return request({
     url: '/v1/ingress?ns=' + ns,
     method: 'get'
+  })
+}
+
+// 创建ingress
+export function createIngress(data) {
+  return request({
+    url: '/v1/ingress',
+    method: 'post',
+    data: data
   })
 }
--- src/views/ingress/create.vue
+++ src/views/ingress/create.vue
@@ -1,13 +1,111 @@
 <template>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>基本配置</span>
+      </div>
+      <el-form :inline="true">
+        <el-form-item label="名称">
+          <el-input v-model="name" placeholder="ingress名称" />
+        </el-form-item>
+        <el-form-item label="命名空间">
+          <el-input v-model="namespace" placeholder="如default" />
+        </el-form-item>
 
+      </el-form>
+    </el-card>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>规则</span>
+      </div>
+      <el-form v-for="(rule,ruleindex) in rules" v-model="rules">
+        <el-form :inline="true">
+          <el-form-item label="域名">
+            <el-input v-model="rule.host" placeholder="填写域名" />
+          </el-form-item>
+          <el-button type="primary" @click="addPathCfg(ruleindex)">+</el-button>
+        </el-form>
+        <template v-for="(pathcfg,pathindex) in rule.paths">
+          <el-form :inline="true">
+            <el-form-item label="Path">
+              <el-input v-model="pathcfg.path" placeholder="Path" />
+            </el-form-item>
+            <el-form-item label="服务名">
+              <el-input v-model="pathcfg.service_name" placeholder="填写Service Name" />
+            </el-form-item>
+            <el-form-item label="端口">
+              <el-input v-model="pathcfg.port" placeholder="填写服务端口" />
+            </el-form-item>
+            <el-form-item>
+              <el-button v-show="pathindex>0" type="primary" @click="rmPathCfg(ruleindex,pathindex)">-</el-button>
+            </el-form-item>
+          </el-form>
+        </template>
+
+      </el-form>
+
+    </el-card>
+
+    <div style="text-align: center;margin-top: 20px">
+      <el-button type="primary" @click="postNew()">保存</el-button>
+    </div>
+  </div>
 </template>
-
 <script>
+import { createIngress } from '@/api/ingress'
+
 export default {
-  name: "create"
+  data() {
+    return {
+      name: '',
+      namespace: '',
+      rules: [
+        {
+          host: '',
+          paths: [
+            {
+              path: '',
+              service_name: '',
+              port: '80'
+            }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    // 新增path 配置
+    addPathCfg(index) {
+      this.rules.forEach((rule, ruleindex) => {
+        if (ruleindex === index) {
+          rule.paths.push({ path: '', service_name: '', port: '80' })
+        }
+      })
+    },
+    // 删除path 配置
+    rmPathCfg(ruleIndex, pathIndex) {
+      this.rules.forEach((rule, ruleindex) => {
+        if (ruleindex === ruleIndex) {
+          rule.paths.forEach((path, pathindex) => {
+            if (pathindex === pathIndex) {
+              rule.paths.splice(pathindex, 1)
+            }
+          })
+        }
+      })
+    },
+    postNew() {
+      const data = {
+        Name: this.name,
+        Namespace: this.namespace,
+        Rules: this.rules
+      }
+      createIngress(data)
+        .then((rsp) => {
+          console.log(rsp.data)
+        })
+    }
+  }
+
 }
 </script>
-
-<style scoped>
-
-</style>
