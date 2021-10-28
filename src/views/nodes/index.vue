520ab18a9787c36960d6a687148afdb952b532f7
神霄三华 <87976956@qq.com>
2021-10-29 00:00:11 +0800

    cpu内存使用率

--- src/views/nodes/index.vue
+++ src/views/nodes/index.vue
@@ -14,11 +14,33 @@
       <el-table-column label="节点名称" align="center">
         <template slot-scope="scope">
           {{ scope.row.name }}
+          <p><el-tag v-for="label in scope.row.labels" :key="label" type="success">{{ label }}</el-tag></p>
+          <p><el-tag v-for="taint in scope.row.taints" :key="taint" type="danger">{{ taint }}</el-tag></p>
         </template>
       </el-table-column>
       <el-table-column label="节点IP" align="center">
         <template slot-scope="scope">
           {{ scope.row.ip }}
+        </template>
+      </el-table-column>
+      <el-table-column label="CPU" align="center">
+        <template slot-scope="scope">
+          {{ Math.round(scope.row.usage.cpu*100) }}% / {{ scope.row.capacity.cpu }}核
+        </template>
+      </el-table-column>
+      <el-table-column label="内存" align="center">
+        <template slot-scope="scope">
+          {{ Math.round(scope.row.usage.memory*100) }}% / {{ Math.round(scope.row.capacity.memory/1000/1000/1000) }}G
+        </template>
+      </el-table-column>
+      <el-table-column label="Pods" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.usage.pods }}/{{ scope.row.capacity.pods }}
+        </template>
+      </el-table-column>
+      <el-table-column label="创建时间" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.created_at }}
         </template>
       </el-table-column>
       <el-table-column label="操作" align="center">
